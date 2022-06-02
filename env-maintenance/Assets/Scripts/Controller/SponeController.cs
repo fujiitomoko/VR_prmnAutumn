639c2a2a998bdff20d84aff01c94552ac3907b86
toyota <cist.b220.s.toyota@gmail.com>
2022-06-02 23:21:15 +0900

    初期配置ランダム化システム完成

--- env-maintenance/Assets/Scripts/Controller/SponeController.cs
+++ env-maintenance/Assets/Scripts/Controller/SponeController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class ResponeController : MonoBehaviour
+public class SponeController : MonoBehaviour
 {
-    [SerializeField] GameObject sponeArea;
     [SerializeField] GameObject sponeObject;
+    BoxCollider col;
+    Vector3 v = new Vector3(0,0,0);
 
     void Start()
     {
-
+        Transform mytransform = this.transform;
+        Vector3 pos = mytransform.position;
+        col = GetComponent<BoxCollider>();
+        v = col.size;
+        float x = Random.Range(pos.x-v.x/2,pos.x+v.x/2);
+        float z = Random.Range(pos.z-v.z/2,pos.z+v.z/2);
+        Instantiate(sponeObject, new Vector3(x,0,z), sponeObject.transform.rotation);
+        Debug.Log(x);
+        Debug.Log(z);
+        Debug.Log(v);
     }
 
     void Update()
