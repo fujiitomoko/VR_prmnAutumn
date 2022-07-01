b0e8135d477fad575fd342d2952a1fd99304229b
unknown <chuanshangxiongji@gmail.com>
2022-07-01 15:12:57 +0900

    ぞうきん持てた

---
+++ env-maintenance/Assets/Scripts/Utility/ZoukinButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoukinButton : MonoBehaviour
+{
+    [SerializeField] GameObject zoukin;
+    bool flag;
+    void Start()
+    {
+        flag = false;
+        zoukin.SetActive(flag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onClick(){
+            Debug.Log(1);
+            flag = !flag;
+            zoukin.SetActive(flag);
+    }
+}
