c4ad951bee8669c0235e407535fc3839eb781f4a
unknown <chuanshangxiongji@gmail.com>
2022-07-01 16:21:04 +0900

    手袋完成

---
+++ env-maintenance/Assets/Scripts/Utility/GloveChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveChange : MonoBehaviour
+{
+
+    public Material[] material;
+    private int a;
+    [SerializeField]GameObject HandR;
+    [SerializeField]GameObject HandL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = 0;
+        HandR.GetComponent<Renderer>().material = material[a];
+        HandL.GetComponent<Renderer>().material = material[a];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onClick(){
+        if(a == 0){
+            a = 1;
+        }
+        else{
+            a = 0;
+        }
+        HandR.GetComponent<Renderer>().material = material[a];
+        HandL.GetComponent<Renderer>().material = material[a];
+    }
+}
